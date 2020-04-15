f649941d8ed6e4b26578b5a3559e7397abf3cd02
Cherry <quany2022@zrtec.net>
2020-04-15 14:07:27 +0800

    翻页爬取

--- getpicture.js
+++ getpicture.js
@@ -5,42 +5,74 @@ const https = require("https"),
     colors = require('colors');
 
 
-
 https.get('https://www.ivsky.com/bizhi/chahua_t1725/', function (res) {
-    var data;
+    var html;
     res.on("data", function (chunk) {
-        data += chunk;
-        console.log(data);
+        html += chunk;
+        console.log(html);
     });
     res.on("end", function () {
-        console.log("<<<----------------------- html响应完毕 ----------------------->>>".green);
-        var $ = cheerio.load(data, {
+        console.log("<<<----------------------- html首次响应完毕 ----------------------->>>".green);
+        var $ = cheerio.load(html, {
             decodeEntities: false
         });
-        var picUrl = [];
-        for (let i = 0; i < $('img').length; i++) {
-            picUrl.push("https:" + $('img')[i].attribs.src);
-        };
-        console.log(picUrl);
-        console.log("<<<----------------------- 目标数据获取完毕 ----------------------->>>".green);
-        //创建images目录，用于装图片
-        fs.mkdir("images", function (err) {
-            if (err) {
-                return console.error(err);
-            }
-            console.log("目录创建成功.开始装入图片...".green);
-            picUrl.forEach((url, idx) => {
-                let PicName = url.split('/').pop(); // 已原网络图片的名称命名本地图片
-                request({
-                    url
-                }).pipe(
-                    fs.createWriteStream(`./images/${PicName}`).on('error', err => {
-                        console.log("写入错误：".red, err.stack);
-                    })
-                );
+        let nth = $('title').text().replace(/[^0-9]/ig, "") / 20;
+        console.log('共有' + nth + '页');
+        for (let index = 1; index <= nth; index++) {
+            https.get(`https://www.ivsky.com/bizhi/chahua_t1725/index_${nth}.html`, function (res) {
+                var data;
+                res.on("data", function (chunks) {
+                    data += chunks;
+                    console.log(data);
+                });
+                res.on("end", function () {
+                    console.log("<<<----------------------- html响应完毕 ----------------------->>>".green);
+                    var $ = cheerio.load(data, {
+                        decodeEntities: false
+                    });
+                    var picUrl = [];
+                    for (let i = 0; i < $('img').length; i++) {
+                        picUrl.push("https:" + $('img')[i].attribs.src);
+                    };
+                    console.log(picUrl);
+                    console.log("<<<----------------------- 目标数据获取完毕 ----------------------->>>".green);
+                    //创建images目录，用于装图片
+                    if (index == 1) {
+                        fs.mkdir("images", function (err) {
+                            if (err) {
+                                return console.error(err);
+                            }
+                            console.log("目录创建成功。开始装入图片...".green);
+                            picUrl.forEach((url, idx) => {
+                                let PicName = url.split('/').pop(); // 已原网络图片的名称命名本地图片
+                                request({
+                                    url
+                                }).pipe(
+                                    fs.createWriteStream(`./images/${PicName}`).on('error', err => {
+                                        console.log("写入错误：".red, err.stack);
+                                    })
+                                );
+                            });
+                            console.log("<<<----------------------- 写入完毕 ----------------------->>>".green);
+                        });
+                    } else {
+                        console.log("开始装入图片...".green);
+                        picUrl.forEach((url, idx) => {
+                            let PicName = url.split('/').pop(); // 已原网络图片的名称命名本地图片
+                            request({
+                                url
+                            }).pipe(
+                                fs.createWriteStream(`./images/${PicName}`).on('error', err => {
+                                    console.log("写入错误：".red, err.stack);
+                                })
+                            );
+                        });
+                    };
+                });
+            }).on("error", function (err) {
+                console.log(err);
             });
-            console.log("<<<----------------------- 写入完毕 ----------------------->>>".green);
-        });
+        };
     });
 }).on("error", function (err) {
     console.log(err);
